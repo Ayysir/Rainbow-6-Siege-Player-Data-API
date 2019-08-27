f3451f67a94f1eac2700c578d440484bdb0e6fdb
Rui Oliveira <39498755+HackerinoS2@users.noreply.github.com>
2019-08-27 18:29:21 +0100

    Updated text

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php 
 
-echo 'Hello, this is a Rainbow 6 Siege Player Data API using R6Tab.com API';
+echo 'Hello, this is a Rainbow 6 Siege Player Data API made by <a href="https://github.com/HackerinoS2" target="_self">Rui Oliveira</a> using R6Tab.com API';
