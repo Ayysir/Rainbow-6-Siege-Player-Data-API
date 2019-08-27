2d1b30ada448e3f40ada769702209647d4b4168e
Rui Oliveira <39498755+HackerinoS2@users.noreply.github.com>
2019-08-27 17:37:21 +0100

    Check Description
    Now using "GetPlayerRankById" in "GetPlayerByName" to be able to get ALL the player data with both Name or Id.
    Added some error handlers.

--- rank.php
+++ rank.php
@@ -1,20 +1,19 @@
 <?php
 
-function GetPlayerRankByName($playerName) {
-
-    $json = file_get_contents('r6ranks.json');
-    $ranksNames = json_decode($json, true);
-    
+function GetPlayerRankByName($playerName) {    
     $playerdatarequest = file_get_contents("https://r6tab.com/api/search.php?platform=uplay&search=".$playerName);
     
     $result = json_decode($playerdatarequest, true);
+
+    if ($result['totalresults'] == 0) {
+        die('No player found');
+    } elseif ($result['totalresults'] > 1) {
+        die('Found '.$result['totalresults'].' players with a similar name. Please try to use the player id instead of player name');
+    }
     
-    $playerRankNumber = $result['results'][0]['p_currentrank'];
-    $playerMMR = $result['results'][0]['p_currentmmr'];
+    $playerId = $result['results'][0]['p_id'];
     
-    $playerRankName = $ranksNames[$playerRankNumber];
-    
-    $response = $playerRankName.' | '.'MMR: '.$playerMMR;
+    $response = GetPlayerRankById($playerId);
 
     return $response;
 }
@@ -27,13 +26,18 @@ function GetPlayerRankById($playerId) {
     $playerdatarequest = file_get_contents("https://r6tab.com/api/player.php?p_id=".$playerId);
     
     $result = json_decode($playerdatarequest, true);
+
+    if ($result['playerfound'] == false) {
+        die('No player found');
+    }
     
     $playerRankNumber = $result['p_EU_rank'];
     $playerMMR = $result['p_EU_currentmmr'];
+    $playerDataLastUpdated = $result['updatedon'];
     
     $playerRankName = $ranksNames[$playerRankNumber];
     
-    $response = $playerRankName.' | '.'MMR: '.$playerMMR;
+    $response = $playerRankName.' | '.'MMR: '.$playerMMR.' | '.$playerDataLastUpdated;
 
     return $response;
 }
